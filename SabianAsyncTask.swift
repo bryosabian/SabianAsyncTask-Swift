6e8130de09c2483fe7ecc44978919422e6412afb
bryosabian <sabianbryo@gmail.com>
2021-09-24 16:18:29 +0300

    Attached main file

---
+++ SabianAsyncTask.swift
@@ -0,0 +1,56 @@
+//
+//  SabianAsyncTask.swift
+//  Nygma Period Tracker
+//
+//  Created by Jared Leto on 20/07/2021.
+//  Copyright © 2021 Jared Leto. All rights reserved.
+//
+
+import Foundation
+
+class SabianAsyncTask<T> {
+    
+    private var ioDispatcher : DispatchQueue {
+        get {
+            return DispatchQueue.global(qos: .background)
+        }
+    }
+    
+   private var defaultDispatcher : DispatchQueue{
+        get {
+            return DispatchQueue.main
+        }
+    }
+    
+    
+    func execute(_ action :  @escaping () throws ->  T, onBefore : (() -> Void)? = nil,  onComplete :  ((T) -> Void)? = nil, onError : ((Error) -> Void)? = nil){
+        
+        onBefore?()
+        
+        var throwable: Error? = nil
+        var retuns : T!
+        
+        let group = DispatchGroup()
+        group.enter()
+        
+        ioDispatcher.async {
+            do{
+            retuns = try action()
+            }catch {
+                throwable = error
+            }
+            group.leave()
+        }
+        
+        group.notify(queue: defaultDispatcher){
+            
+            if throwable != nil {
+                onError?(throwable!)
+            }else{
+                onComplete?(retuns)
+            }
+        }
+        
+    }
+    
+}
